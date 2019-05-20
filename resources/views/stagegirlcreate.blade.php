43cbbb90c5fea9b555a80f4f70a954faef8a2508
diandika <mohammaddicky.andika@gmail.com>
2019-05-20 21:26:39 +0700

    modify form

--- resources/views/stagegirlcreate.blade.php
+++ resources/views/stagegirlcreate.blade.php
@@ -18,22 +18,26 @@
         <div class="row">
           <div class="col-md-4"></div>
           <div class="form-group col-md-4">
+            <label for="query">Query</label>
+            <select name="query_type" id="query_type" class="form-control">
+              <option value="select">Select</option>
+              <option value="update">Update</option>
+              <option value="delete">Delete</option>
+            </select>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="form-group col-md-4">
+            <label for="tabel">Tabel</label><br>
+            <input type="checkbox" name="tabel" value="stagegirl">Stage Girl<br>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="form-group col-md-4">
             <label for="name">Stage Girl Name:</label>
             <input type="text" class="form-control" name="name">
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-4"></div>
-          <div class="form-group col-md-4">
-            <label for="class_num">Class:</label>
-            <input type="text" class="form-control" name="class_num">
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-4"></div>
-          <div class="form-group col-md-4">
-            <label for="weapon">Weapon:</label>
-            <input type="text" class="form-control" name="weapon">
           </div>
         </div>
         <div class="row">
